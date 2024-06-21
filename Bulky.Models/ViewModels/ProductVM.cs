98231ccfbe48cdc9a8331659c64de5954ce4e953
Himanshu Singhal <developer.singhal@gmail.com>
2024-06-21 11:27:35 +0530

    Completed the view model implementation

---
+++ Bulky.Models/ViewModels/ProductVM.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyBook.Models.ViewModels
+{
+    // View Models are also called strongly typed views
+    // These are strongly typed for a view as compared to using viewbag and viewdata
+    public class ProductVM
+    {
+        public Product Product { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+    }
+}
--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -28,20 +29,36 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             });
             // Viewbag transfers data from controller to view and not vice versa
             //ViewBag.CategoryList = CategoryList;
-            ViewData["CategoryList"] = CategoryList;
-            return View();
+            //ViewData["CategoryList"] = CategoryList;
+
+            // View Model is a better approach to pass the data because if the number of viewbag and viewdata increases, it becomes very diffucult to keep track of them
+            ProductVM productVM = new ProductVM()
+            {
+                Product = new Product(),
+                CategoryList = CategoryList
+            };
+            return View(productVM);
         }
         [HttpPost]
-        public IActionResult Create(Product obj)
+        public IActionResult Create(ProductVM obj)
         {
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Product.Add(obj.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully";
                 return RedirectToAction("Index");
             }
-            return View(obj);
+            else 
+            {
+                // Populate the drop down again, it will not populate automatically
+                obj.CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
+                return View(obj);
+            }
         }
 
         public IActionResult Edit(int? id)
